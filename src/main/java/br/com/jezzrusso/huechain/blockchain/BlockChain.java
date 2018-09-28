812c683d2bda2769bb0fce7ffdae3d079a09f1b1
Jessé Pereira Russo <Jesse.russo@zup.com.br>
2018-09-28 15:00:03 -0300

    (Aula 7 - Adicionar método que valida a cadeia de blocos)

--- src/main/java/br/com/jezzrusso/huechain/blockchain/BlockChain.java
+++ src/main/java/br/com/jezzrusso/huechain/blockchain/BlockChain.java
@@ -7,22 +7,40 @@ import java.util.List;
 import br.com.jezzrusso.huechain.block.Block;
 
 public class BlockChain {
-	
-	private final List<Block> chain;;
-	
+
+	private final List<Block> chain;
+
 	public BlockChain() {
 		this.chain = new ArrayList<>();
 		this.chain.add(Block.genesis());
 	}
-	
+
 	public Block addBlock(String data) {
 		Block newBlock = Block.mineBlock(this.chain.get(this.chain.size() - 1).getHash(), data);
 		this.chain.add(newBlock);
 		return newBlock;
 	}
-	
+
 	public List<Block> getBlockChain() {
 		return Collections.unmodifiableList(this.chain);
+	}
+
+	public Boolean isValidChain(final List<Block> chain) {
+		if (!Block.genesis().equals(chain.get(0)))
+			return Boolean.FALSE;
+
+		for (Integer i = 1; i < chain.size(); i++) {
+
+			final Block block = chain.get(i);
+			final Block lastBlock = chain.get(i - 1);
+
+			if (!block.getLastHash().equals(lastBlock.getHash()) || !block.getHash().equals(Block.blockHash(block))) {
+				return Boolean.FALSE;
+			}
+
+		}
+
+		return Boolean.TRUE;
 	}
 
 }
